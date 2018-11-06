b88be0b5f2ffbbaf31d49c5afb758dc9dd73d0d4
EvilTuts <arthur.barakat@epfl.ch>
2018-11-06 18:15:30 +0100

    plot for rating

--- data exploration.py
+++ data exploration.py
@@ -9,19 +9,20 @@ rating = load_csv_data(data_name)
 print(rating.shape)
 x = rating.shape[1]
 print(type(x))
-mean_rating_user = np.zeros(rating.shape[1])
-mean_rating_movie = np.zeros(rating.shape[0])
-print(mean_rating_movie.shape)
-print(mean_rating_user.shape)
-for i in range(len(mean_rating_user)):
-    print(i)
-    mean_rating_user[i] = np.mean(rating[:,i])
-for j in range(len(mean_rating_movie)):
-    print(j)
-    mean_rating_movie[j] = np.mean(rating[j,:])
+#mean_rating_user = np.zeros(rating.shape[1])
+#mean_rating_movie = np.zeros(rating.shape[0])
+#print(mean_rating_movie.shape)
+#print(mean_rating_user.shape)
+
+mean_rating_user = np.mean(rating,axis =0)
+
+mean_rating_movie = np.mean(rating,axis =1)
+
 plt.figure()
 plt.plot(mean_rating_movie)
 plt.title('mean_rating_movie')
+plt.show()
 plt.figure()
-plt.plot(mean_rating_user)
+plt.plot(mean_rating_user.T)
 plt.title('mean_rating_user')
+plt.show()
