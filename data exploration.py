0cd729fc5f6fc5b45de76ccf98c48738408452b6
veillard <jennifer.veillard@epfl.ch>
2018-11-27 16:03:50 +0100

    Data Exploration (still problems)

--- data exploration.py
+++ data exploration.py
@@ -11,13 +11,31 @@ ratings = load_data_sparse(data_name)
 #rating = load_csv_data(data_name)
 
 print(ratings.shape)
-x = ratings.shape[1]
-print(type(x))
+NbrUsers = ratings.shape[1]
+NbrFilm = ratings.shape[1]
+
+#print(ratings[0,0])
+
+#sp.csr_matrix.count_nonzero(ratings)
+
+#import matplotlib.pylab as plt
+#import scipy.sparse as sps
+#A = sps.rand(10000,10000, density=0.00001)
+#M = sps.csr_matrix(ratings)
+#M = sps.csr_matrix(A)
+#plt.spy(M)
+#plt.show()
+
+# Nbr rating per film
+#print(np.count_nonzero(ratings, axis=0))
+
 #mean_rating_user = np.zeros(rating.shape[1])
 #mean_rating_movie = np.zeros(rating.shape[0])
 #print(mean_rating_movie.shape)
 #print(mean_rating_user.shape)
 
+
+"""""
 mean_rating_user = np.mean(ratings,axis =0)
 
 mean_rating_movie = np.mean(ratings,axis =1)
@@ -30,5 +48,6 @@ plt.figure()
 plt.plot(mean_rating_user.T)
 plt.title('mean_rating_user')
 plt.show()
+"""
 
-matrix_factorization_SGD(ratings)
+#matrix_factorization_SGD(ratings)
--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from scipy import sparse
 import numpy as np
+import matplotlib.pyplot as plt
 
-def load_data_sparse(path_dataset):
+def load_data_sparse(path_dataset, exploration = True):
 
     data = pd.read_csv(path_dataset)
     #print(data.head(10))
@@ -11,6 +12,8 @@ def load_data_sparse(path_dataset):
 
     data['c'] = data.Id.str.split('_').str.get(1).str[1:]
     print(data.head(10))
+
+    if exploration : data_exploration(data)
 
     # row indices
     row_ind = np.array(data['r'], dtype=int)
@@ -21,9 +24,79 @@ def load_data_sparse(path_dataset):
 
     # create COO sparse matrix from three arrays
     mat_coo = sparse.coo_matrix((data, (row_ind, col_ind)))
-    print(mat_coo.shape)
+    #print(mat_coo.shape)
 
     mat_lil = mat_coo.tolil()
-    print(mat_lil.shape)
+    #print(mat_lil.shape)
 
     return mat_lil
+
+def data_exploration(data):
+    # 10'000 users and 1'000 films
+
+    #print(data.shape) # (1176952, 4)
+
+    # Nbr Rate Per User
+    NbrRatePerUser = data['r'].value_counts()
+    print('Nbr Rate Per User')
+    print('min: ' + str(np.min(NbrRatePerUser)))
+    print('max: ' + str(np.max(NbrRatePerUser)))
+    print('mean: ' + str(np.mean(NbrRatePerUser)))
+    print('================')
+
+    plt.figure()
+    plt.hist(NbrRatePerUser, bins=1000)
+    plt.xlabel("Nbr Rate Per User")
+    plt.ylabel("Freqeuncy")
+    plt.show()
+
+    # Nbr Rate Per Film
+    NbrRatePerFilm = data['c'].value_counts()
+    #print(NbrRatePerFilm)
+    print('Nbr Rate Per Film')
+    print('min: ' + str(np.min(NbrRatePerFilm)))
+    print('max: ' + str(np.max(NbrRatePerFilm)))
+    print('mean: ' + str(np.mean(NbrRatePerFilm)))
+    print('================')
+
+    plt.figure()
+    plt.hist(NbrRatePerFilm, bins=500)
+    plt.xlabel("Ndr Rate Per Film")
+    plt.ylabel("Freqeuncy")
+    plt.show()
+
+    # Mean Rate Per Film
+    MeanPerFilm = data.groupby('c').mean()
+
+    """plt.figure()
+    plt.plot(MeanPerFilm)
+    plt.xlabel("Film")
+    plt.ylabel("Mean")
+    plt.show()"""
+
+    print(MeanPerFilm.shape)
+
+    MeanPerFilm_rounded = round(MeanPerFilm,1)
+    print(MeanPerFilm_rounded)
+    MeanPerFilm_count = MeanPerFilm_rounded.value_counts()
+
+    plt.figure()
+    plt.hist(MeanPerFilm_count,range=[1,5])#bins='auto',range=[1,5])
+    plt.xlabel("Mean Per Film")
+    plt.ylabel("Freqeuncy")
+    plt.show()
+
+    """"# Variance in Rating Per Film
+    StdPerFilm = data.groupby('c').std()
+
+    plt.figure()
+    plt.plot(StdPerFilm)
+    plt.xlabel("Film")
+    plt.ylabel("Std")
+    plt.show()
+
+    plt.figure()
+    plt.hist(StdPerFilm, bins=5)
+    plt.xlabel("Variance Per Film")
+    plt.ylabel("Freqeuncy")
+    plt.show()"""
