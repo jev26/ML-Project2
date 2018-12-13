67c98c7f6e84345a73d019a5d86e297db07f58d1
Loic Nguyen <loic.nguyen@epfl.ch>
2018-12-13 12:31:20 +0100

    change return of simple models

--- simple_models.py
+++ simple_models.py
@@ -1,14 +1,6 @@
 import numpy as np
-import pandas as pd
 
-
-def calculate_mse(real_label, prediction):
-    """calculate MSE."""
-    t = real_label - prediction
-    return 1.0 * t.dot(t.T)
-
-
-def baseline_global_mean(train, test):
+def global_mean(train, test):
     """baseline method: use the global mean."""
     # find the non zero ratings in the train
     nonzero_train = train[train.nonzero()]
@@ -16,21 +8,15 @@ def baseline_global_mean(train, test):
     # calculate the global mean
     global_mean_train = nonzero_train.mean()
 
-    # find the non zero ratings in the test
-    nonzero_test = test[test.nonzero()].todense()
-
-    # predict the ratings as global mean
-    mse = calculate_mse(nonzero_test, global_mean_train)
-    rmse = np.sqrt(1.0 * mse / nonzero_test.shape[1])
-    print("test RMSE of baseline using the global mean: {v}.".format(v=rmse))
+    return np.full(train.shape, global_mean_train)
 
 
 
 
-def baseline_user_mean(train, test):
+def user_mean(train, test):
     """baseline method: use the user means as the prediction."""
-    mse = 0
     num_items, num_users = train.shape
+    predictions = np.zeros(train.shape)
 
     for user_index in range(num_users):
         # find the non-zero ratings for each user in the training dataset
@@ -40,23 +26,15 @@ def baseline_user_mean(train, test):
         # calculate the mean if the number of elements is not 0
         if nonzeros_train_ratings.shape[0] != 0:
             user_train_mean = nonzeros_train_ratings.mean()
-        else:
-            continue
+            predictions[:, user_index] = user_train_mean
 
-        # find the non-zero ratings for each user in the test dataset
-        test_ratings = test[:, user_index]
-        nonzeros_test_ratings = test_ratings[test_ratings.nonzero()].todense()
 
-        # calculate the test error
-        mse += calculate_mse(nonzeros_test_ratings, user_train_mean)
-    rmse = np.sqrt(1.0 * mse / test.nnz)
-    print("test RMSE of the baseline using the user mean: {v}.".format(v=rmse))
-
+    return predictions
 
 def baseline_item_mean(train, test):
     """baseline method: use item means as the prediction."""
-    mse = 0
     num_items, num_users = train.shape
+    predictions = np.zeros(train.shape)
 
     for item_index in range(num_items):
         # find the non-zero ratings for each item in the training dataset
@@ -66,14 +44,6 @@ def baseline_item_mean(train, test):
         # calculate the mean if the number of elements is not 0
         if nonzeros_train_ratings.shape[0] != 0:
             item_train_mean = nonzeros_train_ratings.mean()
-        else:
-            continue
+            predictions[item_index, :] = item_train_mean
 
-        # find the non-zero ratings for each movie in the test dataset
-        test_ratings = test[item_index, :]
-        nonzeros_test_ratings = test_ratings[test_ratings.nonzero()].todense()
-
-        # calculate the test error
-        mse += calculate_mse(nonzeros_test_ratings, item_train_mean)
-    rmse = np.sqrt(1.0 * mse / test.nnz)
-    print("test RMSE of the baseline using the item mean: {v}.".format(v=rmse))
+    return predictions
