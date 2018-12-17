5ed26d1ea0e986235f9e861df0503d64ac01cbc1
Loic Nguyen <loic.nguyen@epfl.ch>
2018-12-17 16:32:55 +0100

    fix bug

--- surprise_models.py
+++ surprise_models.py
@@ -8,10 +8,12 @@ def pandas_to_surprise(data, pred=False):
     data_spr = spr.Dataset.load_from_df(data[['User', 'Movie', 'Rating']], reader)
     if pred:
         data_spr = data_spr.build_full_trainset().build_testset()
+        data_spr = np.sorted(data_spr, key=lambda x: (x[1], x[0]))
     return data_spr
 
 def get_testset_indices(test_spr):
-    return list(zip(*test_spr))
+    users_idx, movies_idx, labels = list(zip(*test_spr))
+    return users_idx, movies_idx, labels
 
 def testset_to_sparse_matrix(test_spr, pred=False):
     matrix = sp.lil_matrix((10000, 1000))
